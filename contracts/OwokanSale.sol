3a00abc388f9b8644fd17d00ceb6ad97cc66e3fd
qp <dabslant@gmail.com>
2019-06-04 13:37:16 +0100

    Buy Tokens Implemented

--- contracts/OwokanSale.sol
+++ contracts/OwokanSale.sol
@@ -4,28 +4,53 @@ import "./Owokan.sol";
 
 contract OwokanSale {
     // provide token to token sale contract
-    // buy token
     // end sale
 
-    // admin
-    // not exposed to the public
     address admin;
-
-    // token contrat
     Owokan public tokenContract;
-
-    // token price
     uint256 public tokenPrice;
+
+    // token solidity
+    uint256 public tokenSold;
+
+    // sell event
+    event Sell(address _buyer, uint256 _amount);
 
 
     constructor(Owokan _tokenContract, uint256 _tokenPrice) public {
-        // asign an admin
         admin = msg.sender;
-
-        // assign token contract
         tokenContract = _tokenContract;
-
-        // set token price
         tokenPrice = _tokenPrice;
+    }
+
+    // multiply numberOfTokens and tokenPrice
+    /*DS_Math:
+
+        function mul(uint x, uint y) internal pure returns (uint z) {
+            require(y == 0 || (z = x * y) / y == x,                 "ds-math-mul-overflow");
+        }*/
+    function multiply(uint x, uint y) internal pure returns (uint z) {
+        require(y == 0 || (z = x * y) / y == x);
+    }
+
+    // buy tokens
+    function buyTokens(uint256 _numberOfTokens) public payable {
+        // require the value is equal to tokens
+        require(msg.value == multiply(_numberOfTokens, tokenPrice));
+
+        // require contract has enough tokens available
+        // use this to referece this contrat
+        // it returns owokanSale contract address
+        /* address myAddress = address(this); */
+        require(tokenContract.balanceOf(address(this)) >= _numberOfTokens);
+
+        // require a transfer is successful
+        require(tokenContract.transfer(msg.sender, _numberOfTokens));
+
+        // track number of tokens sold
+        tokenSold += _numberOfTokens;
+
+        // trigget a sell event
+        emit Sell(msg.sender, _numberOfTokens);
     }
 }
--- test/OwokanSale.js
+++ test/OwokanSale.js
@@ -1,9 +1,15 @@
+const Owokan = artifacts.require('Owokan.sol');
 const OwokanSale = artifacts.require('OwokanSale.sol');
 
 contract('OwokanSale', function(accounts) {
 
+    var owokanInstance;
     var owokanSaleInstance;
+    const admin = accounts[0];
+    const buyer = accounts[1];
     const tokenPrice = 1000000000000000; // in wei
+    const tokensAvailable = 750;
+    var numberOfTokens;
 
     it('initilizes contract with the correct values', function() {
         return OwokanSale.deployed().then(function(instance) {
@@ -20,4 +26,43 @@ contract('OwokanSale', function(accounts) {
         });
     });
 
-})
+    it('facilitates token purchase', function() {
+        return Owokan.deployed().then(function(instance) {
+            // grab owokan instance first
+            owokanInstance = instance;
+            return OwokanSale.deployed();
+        }).then(function(instance) {
+            // grab owokanSale instance
+            owokanSaleInstance = instance;
+
+            // transfer 75% of all token to owokanSale contract
+            return owokanInstance.transfer(owokanSaleInstance.address, tokensAvailable, { from: admin });
+        }).then(function(receipt) {
+            numberOfTokens = 10;
+            return owokanSaleInstance.buyTokens(numberOfTokens, { from: buyer, value: numberOfTokens * tokenPrice });
+        }).then(function(receipt) {
+            assert.equal(receipt.logs.length, 1, "an event is triggered");
+            assert.equal(receipt.logs[0].event, "Sell", "the event type is of 'Sell'");
+            assert.equal(receipt.logs[0].args._buyer, buyer, "logs the account that bought tokens");
+            assert.equal(receipt.logs[0].args._amount, numberOfTokens, "logs the number of tokens bought");
+            return owokanSaleInstance.tokenSold();
+        }).then(function(amount) {
+            assert.equal(amount.toNumber(), numberOfTokens, ' increments the number of tokens sold');
+            return owokanInstance.balanceOf(owokanSaleInstance.address);
+        }).then(function(balance) {
+            assert.equal(balance.toNumber(), tokensAvailable - numberOfTokens, 'balance is available');
+            return owokanInstance.balanceOf(buyer);
+        }).then(function(balance) {
+            assert.equal(balance.toNumber(), numberOfTokens, 'balance is available');
+            // try to buy token at a different price
+            return owokanSaleInstance.buyTokens(numberOfTokens, { from: buyer, value: 1 });
+        }).then(assert.fail).catch(function(error) {
+            assert(error.message.indexOf('revert') >= 0, 'msg.value must equal number of tokens in wei');
+            numberOfTokens = 1000;
+            return owokanSaleInstance.buyTokens(numberOfTokens, { from: buyer, value: numberOfTokens * tokenPrice });
+        }).then(assert.fail).catch(function(error) {
+            assert(error.message.indexOf('revert') >= 0, 'cannot purchase more than available tokens');
+        });
+    });
+
+});``
